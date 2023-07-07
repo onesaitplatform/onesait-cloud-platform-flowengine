c6e7dc3eb5a4ea3f0b1aa1e3f6094ffe4102dc86
echico <echico@minsait.com>
2023-07-07 15:43:45 +0200

    New release 5.0.0-ce

--- Flow-Engine-Manager/onesait-platform/nodes/config/onesait-platform-config.js
+++ Flow-Engine-Manager/onesait-platform/nodes/config/onesait-platform-config.js
@@ -1,9 +1,8 @@
 var onesaitPlatformConfig = {
-    "sibBasePath":"http://localhost:8080",
+    "sibBasePath":"http://iotbrokerservice:19000",
     "scriptBasePath":"http://localhost:20100",
-    "notebooks":"http://localhost",
-	"consoleBasePath":"http://localhost:18000",
-	"digitalTwinBasePath":"http://localhost:19300"
+    "consoleBasePath":"http://controlpanelservice:18000",
+    "digitalTwinBasePath":"http://digitaltwinbrokerservice:19300"
 }
 
 module.exports = onesaitPlatformConfig;
